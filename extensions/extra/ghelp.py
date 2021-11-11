5a248edc20f799d5e65733724f580282f6b083f4
Boby <40067018+bobyclaws@users.noreply.github.com>
2021-11-11 21:12:51 +0530

    some refactoring, fix double tap to navigate embed page

--- extensions/extra/ghelp.py
+++ extensions/extra/ghelp.py
@@ -1,48 +1,72 @@
+import json
+import os
+import asyncio
 from nextcord.ext import commands
+from nextcord import Embed
+from nextcord.message import Message
+from nextcord.reaction import Reaction
 
-from util.logging import logc
+from core.paimon import Paimon
 
-from base.help import GenshinHelp
 
-help_handler = GenshinHelp()
+class GenshinHelp(commands.Cog):
+    def __init__(self, pmon):
+        self.pmon: Paimon = pmon
 
-class GHelp(commands.Cog):
-    def __init__(self, client):
-        self.client = client
 
-    
+        # load embed data.
+        embed_data = 'assets/GenshinHelp/embed_data.json'
+        if os.path.exists(embed_data):
+            with open(embed_data,'r') as f:
+                embed_data = json.load(f)
+
+            # preload all embeds.
+            self.embeds = []
+            for i in embed_data:
+                embed = Embed.from_dict(embed_data[i])
+                self.embeds.append(embed)
+
+
 
     @commands.command(aliases=['help', 'h'])
-    async def ghelp(self, ctx):    
-        stop = False
-        select = 0
-        embeds,emojis = help_handler.create_embeds()
-        count = len(embeds)
-        msg = await ctx.send(embed=embeds[0])
-        for i in emojis:
+    async def ghelp(self, ctx):
+
+        embed_select = 0
+        count = len(self.embeds)
+        msg: Message = await ctx.send(embed=self.embeds[0])
+        
+        # add nav butons
+        for i in ['⬅️','➡️']:
             await msg.add_reaction(i)
-        while stop == False:
+
+        while True:
             try:
-                def check(reaction,user):
-                    return reaction.message.id == msg.id and user == ctx.author
-                reaction, user = await self.client.wait_for('reaction_add',check=check,timeout=60)
-            except TimeoutError:
+                reaction: Reaction = None
+                reaction, _ = await self.pmon.wait_for(
+                    'reaction_add',
+                    timeout=6,
+                    check=(lambda reaction, user:
+                           reaction.message.id == msg.id and user == ctx.author))
+
+                await msg.remove_reaction(reaction, ctx.author)
+                
+            except asyncio.exceptions.TimeoutError:
                 await msg.clear_reactions()
-            else:                    
-                if reaction.emoji in emojis:                
-                    if reaction.emoji == '➡️':
-                        if select < count-1:
-                            select += 1
-                            await msg.edit(embed=embeds[select])                           
-                    if reaction.emoji == '⬅️':
-                        if select >= 1:
-                            select -= 1
-                            await msg.edit(embed=embeds[select])
+                return
+            else:                                 
+                if reaction.emoji == '➡️':
+                    if embed_select < count-1:
+                        embed_select += 1
+                        await msg.edit(embed=self.embeds[embed_select])                           
+                if reaction.emoji == '⬅️':
+                    if embed_select >= 1:
+                        embed_select -= 1
+                        await msg.edit(embed=self.embeds[embed_select])
 
 
 def setup(bot):
-    bot.add_cog(GHelp(bot))
+    bot.add_cog(GenshinHelp(bot))
 
 
 def teardown(bot):
-    bot.remove_cog("GHelp")
+    bot.remove_cog("GenshinHelp")
