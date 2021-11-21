2673f04d9c906a1ca4e2cae68bb8e6c9ada960b1
Reko <alitahir358@gmail.com>
2021-11-21 22:58:22 +0500

    ported bump to cog

---
+++ extensions/extra/bump.py
@@ -0,0 +1,90 @@
+from nextcord.ext import commands
+from nextcord import Embed, File,Member
+from nextcord.ui import View
+from nextcord.utils import get
+
+from base.bump import Bump
+
+from core.paimon import Paimon
+
+
+class Bumper(commands.Cog):
+    def __init__(self, pmon : Paimon):
+        '''
+        initializes bump cog
+        '''
+        self.pmon = pmon
+        self.bump_handler = Bump(pmon)
+
+    @commands.Cog.listener()
+    async def on_message(self,message):
+        '''
+        look for bump message if successful sets a reminder to set message
+        '''
+
+        await self.bump_handler.parse_message_for_bump(message)
+    
+    @commands.command()
+    async def bumps(self, ctx, user: Member = None):
+        if user == None:
+            user = ctx.author
+
+        bumps = self.bump_handler.get_bump_counter(str(ctx.author.id))
+
+        if bumps:
+            embed = Embed(title='Bumps', 
+                        description=f'bumped this server {bumps} times!',
+                        color=0xf5e0d0) 
+            embed.set_author(name=user.display_name,
+                                icon_url=user.avatar.url)
+            await ctx.send(embed=embed)
+
+        else:
+            embed = Embed(title='Bumps', 
+                        description=f'have never bumped this server!',
+                        color=0xf5e0d0) 
+
+            embed.set_author(name=user.display_name,
+                                icon_url=user.avatar.url)
+
+            await ctx.send(embed=embed)
+
+    @commands.command(aliases=['bumpt','bt'])
+    async def bumptop(self,ctx):
+
+        top_bumper = self.bump_handler.get_topbumper()
+        bumps = self.bump_handler.get_bump_counter(str(top_bumper))
+
+        user = get(ctx.guild.members,id=int(top_bumper))
+
+        if top_bumper == None:
+            if user == None:
+                embed = Embed(title='Top Bumper',
+                        description=f'have bumped this server most time\n around {bumps} times!',
+                        color=0xf5e0d0) 
+                embed.set_author(name=user.display_name,
+                                icon_url=user.avatar.url)
+
+                await ctx.send(embed=embed)
+            else:
+                embed = Embed(title='Error', 
+                            description=f'Could not find any top bumper',
+                            color=0xf5e0d0) 
+
+                embed.set_author(name=user.display_name,icon_url=user.avatar.url)
+                await ctx.send(embed=embed)
+        else:
+            embed = Embed(title='Error', 
+                            description=f'Could not find any top bumper',
+                            color=0xf5e0d0) 
+
+            embed.set_author(name=user.display_name,icon_url=user.avatar.url)
+            await ctx.send(embed=embed)
+
+
+def setup(bot):
+    bot.add_cog(Bumper(bot))
+
+
+def teardown(bot):
+    bot.remove_cog("Bumper")
